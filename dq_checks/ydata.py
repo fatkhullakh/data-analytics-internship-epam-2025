69306c09e7daa8d98f6fdf57d9b284801c6289c7
Fatkhullakh <76446631+fatkhullakh@users.noreply.github.com>
2025-09-29 23:05:05 +0500

    Delete dq_checks/ydata.py

--- dq_checks/ydata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-df = pd.read_csv('car_prices.csv')
-profile = ProfileReport(df, title="Car Prices Profiling", explorative=True)
-profile.to_file('car_prices_profile.html')
